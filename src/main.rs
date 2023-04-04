f13f68210911ee0698430e24b84bcfca3ea2497b
Gandalf the grey <112687561+Harsha-vardhan-R@users.noreply.github.com>
2023-04-04 18:06:53 +0530

    Modulated into functions.
    to improv the readability of the user.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
 use image::RgbImage;
-use mandelbrot::no_of_bounces;
+use mandelbrot::{no_of_bounces, pixelcolour};
 use num::complex::Complex;
 
 fn main() {
-    //Setting the dimensions of the out put photograph.
-    //Just set the width it automatically takes in the 
-    //height value by considering a 16:9 aspect ratio.
-    let width = 3000;
-    let height = width * ( 9 / 16);
+    /*
+    Setting the dimensions of the out put photograph.
+    Set the width, it automatically takes in the 
+    height value by considering a 16:9 aspect ratio.
+    */
+    let width = 1000;
+    let height = (width as f32 * (9 as f32 / 16 as f32))as u32;
     
     let mut imgbuffer = RgbImage::new(width, height);
       
     loop {
 
-        'outer: for x in 0..width {
+        for x in 0..width {
             for y in 0..height{
                 //For adjusting the center and scale.
                 let osrx = -1.2;
@@ -24,19 +26,21 @@ fn main() {
 
 
                 let c = Complex::new(cx, cy);
-                let i = no_of_bounces(c, 255) as u8;
+                let i = no_of_bounces(c, 255);
 
-                let colourofpixel = image::Rgb([i * 0.3 as u8, i as u8,i * 0.2 as u8]);
+                let (cr, cg, cb) = pixelcolour(i);
+                let colourofpixel = image::Rgb([cr , cg , cb]);
                 imgbuffer.put_pixel(x, y, colourofpixel);
 
-                }
-            
             }
-            let _path = format!("havaqq{}.png",count);
-            imgbuffer.save(_path).unwrap();
+            
+        }
+
+        let _path = format!("Image.png");
+        imgbuffer.save(_path).unwrap();
         
-        
-            break 'outer
+        break;
+            
     }
     
 }
