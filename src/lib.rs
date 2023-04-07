78b27df64fd87d732960f17922b15eb84ab67130
Gandalf the grey <112687561+Harsha-vardhan-R@users.noreply.github.com>
2023-04-08 01:14:43 +0530

    Now stable without overflow.
    Added modulation to the code to help further development.

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,41 @@
 #[allow(non_snake_case)]
-use num::complex::Complex;
 
-/*
-This function gives out the count value, which is the number of bounces(iterations) 
-it takes to get out of the threshold vlue(point of no return)(here it is 2)
-so if a input number has more bounces it means it is nearer to converging or it converges. 
-Distintion takes the maximum number of bounces to look before it escapes.
-*/
-pub fn no_of_bounces(input_complex: num::complex::Complex64,distinction: u16) -> u16 {
 
-    let mut count = 0;
-    let mut z = Complex::new(0.0, 0.0);
+pub mod pixel_parameters {
 
-    while count <= distinction && z.norm() < 2.0  {
-        //Can be changed for different fractals,but also need to change the center at the same time.
-        z = (z * z) + input_complex;
-        count += 1;
+    use num::complex::Complex;
+    /*
+    This function gives out the count value, which is the number of bounces(iterations) 
+    it takes to get out of the threshold vlue(point of no return)(here it is 2)
+    so if a input number has more bounces it means it is nearer to converging or it converges. 
+    Distintion takes the maximum number of bounces to look before it escapes.
+    */
+    pub fn no_of_bounces(input_complex: num::complex::Complex64,distinction: u16) -> u16 {
+
+        let mut count = 0;
+        let mut z = Complex::new(0.0, 0.0);
+
+        while count <= distinction && z.norm() < 2.0  {
+            //Can be changed for different fractals,but also need to change the center at the same time.
+            z = (z * z) + input_complex;
+            count += 1;
+        }
+
+        count
+
     }
 
-    count
+    
+    //Take in a single value return three values for r,g,bcomponents to maintain a proper colour palatte.
+    pub fn pixelcolour(para: u16) -> (u8 , u8 , u8) {
 
-}
+        let para = para as u8;
+        let r = para;
+        let g = para;
+        let b = 255 - para;
 
+        (r , g , b)
 
-/*
-Take in a single value return three values for r,g,b
-components to maintain a proper colour palatte.
-*/
-pub fn pixelcolour(Para: u16) -> (u8 , u8 , u8) {
-
-    let r = Para as u8;
-    let g = Para as u8;
-    let b = Para as u8;
-
-    (r , g , b)
+    }
 
 }
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,32 @@
 #[allow(non_snake_case,unused_mut,unused_assignments)]
 use image::RgbImage;
-use mandelbrot_animator::{no_of_bounces, pixelcolour};
 use num::complex::Complex;
-//Images will be stored in "Rendered_images_here" folder.
-fn main() {
+use mandelbrot_animator::pixel_parameters::*;
+use std::time;
 
+
+fn main() -> ! {
+
+    //Images will be stored in "Rendered_images_here" folder.
+    let path = "Rendered_images_here";
     //1:1 Aspect ratio.
     let dimen = 1000;
     //which point you want to be zoomed in(The zoom in boundary point)--the given point is one of the interesting points.
     let (fixpnt_x, fixpnt_y) = (-0.743643887037158704752191506114774 , 0.131825904205311970493132056385139);
+    //Initial fixed off-sets and range, Not to be disturbed if you want a full image at the start of the animation.
     let fosx = -1.8;
     let fosy = -1.15;
     let mut range = 2.3_f64;
     let mut pivot_point = (fosx , fosy);
+    let distinction = 255_u16;
+    
+    let mut frame = 1_u16;
 
-    let mut frame = 1_u8;
-
-    //loop for creating an animation by changing parameters little by little.
+    //loop for creating an animation, Need to stop it manually by pessing ctrl + c or cmd + c in the terminal.
     loop {
 
+        let start_time = time::Instant::now();
         let mut imgbuffer = RgbImage::new(dimen, dimen);
-
         let gradient = range / (dimen as f64);
 
         for x in 0..dimen {
@@ -30,9 +36,8 @@ fn main() {
                 //Nothing much to change here.
                 let cx = pivot_point.0 + (gradient * (x as f64));
                 let cy = pivot_point.1 + (gradient * (y as f64));
-
                 let c = Complex::new(cx, cy);
-                let i = no_of_bounces(c, 255);
+                let i = no_of_bounces(c, distinction);
                 let (cr, cg, cb) = pixelcolour(i);
                 let colourofpixel = image::Rgb([cr , cg , cb]);
                 imgbuffer.put_pixel(x, y, colourofpixel);
@@ -41,14 +46,13 @@ fn main() {
             
         }
 
-        let path = format!("Rendered_images_here/Fractal resolution-{} frame-{}.png", dimen , frame);
+        let path = format!("{}/Fractal resolution-{} frame-{}.png",path, dimen , frame);
         imgbuffer.save(path).unwrap();
         /*
-        This controls the speed/smoothness of the animation,
-        should be inbetween exclusively 0.0 and 1.0,
+        This controls the speed/smoothness of the animation,should be inbetween exclusively 0.0 and 1.0,
         nearer values to 1 make a smoother animation.
         */
-        let k = 0.97;
+        let k = 0.99;
         range = range * k;
         pivot_point.0 = fixpnt_x - (k * (fixpnt_x - pivot_point.0));
         pivot_point.1 = fixpnt_y - (k * (fixpnt_y - pivot_point.1));
@@ -56,6 +60,10 @@ fn main() {
         frame += 1;
         
         //break;//If you only want one frame.
+        let end_time = time::Instant::now();
+        let duration = end_time - start_time;
+        //This debug is to know the amount of time taken to render the present frame;
+        dbg!(duration);
 
     }
     
